cdbaa11d3bcb2d47cc894ef9d15ca6e8cffffa3f
Eden-Cohen1 <edencohen.dev@gmail.com>
2025-02-01 11:25:20 +0200

    Fix: Use Azure-assigned port

--- server-python/app.py
+++ server-python/app.py
@@ -38,4 +38,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from Azure
+    app.run(host="0.0.0.0", port=port, debug=True)
