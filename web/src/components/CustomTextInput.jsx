c8bf297a594547db7643bb66587dabba73bfadd2
Eden-Cohen1 <edencohen.dev@gmail.com>
2025-02-04 17:48:07 +0200

    cleaned code, inserted comments

--- web/src/components/CustomTextInput.jsx
+++ web/src/components/CustomTextInput.jsx
@@ -8,14 +8,24 @@ function CustomTextInput({
   errors = [],
   warnings = [],
 }) {
+  // State to manage visibility for password fields
   const [showPassword, setShowPassword] = useState(false);
+
+  // Determine if the input is a password field
   const isPass = type === "password";
+
+  // Choose the appropriate icon based on the input type
   const icon = isPass ? "/lock.svg" : "/email.svg";
+
+  // Set input type: show text if password is visible, otherwise hide it
   const inputType = isPass ? (showPassword ? "text" : "password") : type;
 
   return (
     <div className="mb-4 flex gap-3 items-center relative">
+      {/* Display input icon */}
       <img src={icon} className="absolute ml-2" />
+
+      {/* If password field, show toggle button */}
       {isPass && (
         <button
           type="button"
@@ -25,6 +35,8 @@ function CustomTextInput({
           <img src="/view.svg" />
         </button>
       )}
+
+      {/* Input field */}
       <input
         type={inputType}
         className="w-full pl-10 py-2 text-xs border-[1px] border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-offset ring-offset-[1px] ring-offset-primary focus:shadow-4xl focus:shadow-blue-200/50 transition"
@@ -32,6 +44,8 @@ function CustomTextInput({
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
+
+      {/* Display errors and warnings if any */}
       {(errors.length > 0 || warnings.length > 0) && (
         <div className="absolute -bottom-4 left-1 text-xs">
           {errors.map((error, i) => (
